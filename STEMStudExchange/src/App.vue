c23c7d773a418dbce07444199f8af011fa0a7dc7
jjdubski <101855602+jjdubski@users.noreply.github.com>
2022-11-05 15:13:47 -0500

    main setup

--- STEMStudExchange/src/App.vue
+++ STEMStudExchange/src/App.vue
@@ -1,19 +1,16 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './components/HomeView.vue';
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    <a href="" target="_blank">
+      <img src="./assets/candy.png" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <HomeView msg="The Candy Exchange" />
   </div>
-  <HelloWorld msg="The Candy Exchange" />
 </template>
 
 <style scoped>
--- STEMStudExchange/src/components/HomeView.vue
+++ STEMStudExchange/src/components/HomeView.vue
@@ -1,18 +1,20 @@
 <script setup>
 import { ref } from 'vue'
+import newPost from './newPost.vue';
 
 defineProps({
   msg: String
 })
 
-const count = ref(0)
+
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+      <button type="button" @click="show=true">+ New Post</button>
+    
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
