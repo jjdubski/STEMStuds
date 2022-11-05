3dc90d5e9abae33b8f1dd4ec22a5275e56144e68
Tczerwien <112604547+Tczerwien@users.noreply.github.com>
2022-11-05 16:31:05 -0500

    Update Main.java

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("bro!");
         System.out.println("something else");
+        System.out.println("WORK PLZ!");
     }
 }
