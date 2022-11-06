c45be4c794be7c7ca20e32a7081e59ae42674bff
Tczerwien <112604547+Tczerwien@users.noreply.github.com>
2022-11-05 21:13:18 -0500

    Added Balls

---
+++ Java/src/CandyEntry.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class CandyEntry {
+    private int numOnHand, numDesired;
+    private String name, onHand, desired;
+    private static int entryNumber = 0;
+    private static ArrayList<CandyEntry> entryList;
+
+    public CandyEntry(String name, int numOnHand, String onHand, int numDesired, String desired){
+        entryNumber ++;
+        this.name = name;
+        this.numOnHand = numOnHand;
+        this.onHand = onHand;
+        this.numDesired = numDesired;
+        this.desired = desired;
+    }
+
+   // public String toString(){
+       // return "Entry Number: " + entryNumber
+ //   }
+
+
+}
